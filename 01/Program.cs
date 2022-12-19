312cc72907b0e43704344249189c807882102e91
avkirilin <nerosashok@mail.ru>
2022-12-19 12:52:29 +0300

    Оптимизация алгоритма решения первой задачи

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,6 +1,5 @@
 ﻿int Num = new Random().Next(100,1000);
 int a = Num / 100;
-int b = Num / 10;
-int c = Num % 10;
+int b = Num % 10;
 Console.WriteLine($"{Num}"); 
-Console.WriteLine($"{a}{c}"); 
+Console.WriteLine($"{a}{b}"); 
