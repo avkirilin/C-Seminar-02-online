8fcb669c5c05994a517a450adc3bb00db581b7a1
avkirilin <nerosashok@mail.ru>
2022-12-19 12:57:35 +0300

    Оптимизация алгоритма решения четвертой задачи

--- 04/Program.cs
+++ 04/Program.cs
@@ -4,9 +4,9 @@ Console.WriteLine("Введите второе число");
 int secondNum = Convert.ToInt32(Console.ReadLine());
 if ((firstNum*firstNum) % secondNum == 0 || (secondNum*secondNum) % firstNum == 0)
 {
-    Console.WriteLine($"Кратно");
+    Console.WriteLine("Кратно");
 }
 else
 {
-    Console.WriteLine($"Не кратно");
+    Console.WriteLine("Не кратно");
 }
